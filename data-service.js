c73897f24826e0f96d8435678d317c2f386183e2
Fernando Proto <fernando_proto@hotmail.com>
2017-06-07 19:29:21 -0400

    Changes for Assignment 3

---
+++ data-service.js
@@ -0,0 +1,128 @@
+const fs = require('fs');
+var employees = [];
+var departments = [];
+
+module.exports.initialize = function () {
+    return new Promise((resolve, reject) => {
+        fs.readFile('./data/employees.json', (err, data) => {
+            if (err)
+                reject(err);
+            else
+                employees = JSON.parse(data);
+
+            fs.readFile('./data/departments.json', (err, data) => {
+                if (err)
+                    reject(err);
+                else
+                    departments = JSON.parse(data);
+                if (employees.length == 0 || departments.length == 0)
+                    reject("Unable to read file");
+                else
+                    resolve();
+            });
+        });
+    });
+};
+
+
+module.exports.getAllEmployees = function () {
+    return new Promise((resolve, reject) => { // place our code inside a "Promise" function
+        if (employees.length == 0) {
+            reject("no results returned");
+        } else {
+            resolve(employees); // call "resolve" because we have completed the function successfully
+        }
+    });
+}
+
+module.exports.getEmployeesByStatus = function (status) {
+    var employeesTemp = [];
+    return new Promise((resolve, reject) => {
+        for (var i = 0; i < employees.length; i++) {
+            if (employees[i].status == status) {
+                employeesTemp.push(employees[i])
+            }
+        }
+        if (employeesTemp.length == 0) {
+            reject("no results returned");
+        } else {
+            resolve(employeesTemp); // call "resolve" because we have completed the function successfully
+        }
+    });
+}
+
+module.exports.getEmployeesByDepartment = function (department) {
+    var employeesTemp = [];
+    return new Promise((resolve, reject) => {
+        for (var i = 0; i < employees.length; i++) {
+            if (employees[i].department == department) {
+                employeesTemp.push(employees[i])
+            }
+        }
+        if (employeesTemp.length == 0) {
+            reject("no results returned");
+        } else {
+            resolve(employeesTemp); // call "resolve" because we have completed the function successfully
+        }
+    });
+}
+
+module.exports.getEmployeesByManager = function (manager) {
+    var employeesTemp = [];
+    return new Promise((resolve, reject) => {
+        for (var i = 0; i < employees.length; i++) {
+            if (employees[i].manager == manager) {
+                employeesTemp.push(employees[i])
+            }
+        }
+        if (employeesTemp.length == 0) {
+            console.log("ERROR");
+            reject("no results returned");
+        } else {
+            resolve(employeesTemp); // call "resolve" because we have completed the function successfully
+        }
+    });
+}
+
+module.exports.getEmployeeByNum = function (num) {
+    var employeesTemp = "";
+    return new Promise((resolve, reject) => {
+        for (var i = 0; i < employees.length; i++) {
+            if (employees[i].employeeNum == num) {
+                employeesTemp = employees[i];
+                break;
+            }
+        }
+        if (employeesTemp == "") {
+            reject("no results returned");
+        } else {
+            resolve(employeesTemp); // call "resolve" because we have completed the function successfully
+        }
+    });
+}
+
+module.exports.getManagers = function () {
+    var employeesTemp = [];
+    return new Promise((resolve, reject) => {
+        for (var i = 0; i < employees.length; i++) {
+            if (employees[i].isManager == true) {
+                employeesTemp.push(employees[i])
+            }
+        }
+        if (employeesTemp.length == 0) {
+            reject("no results returned");
+        } else {
+            resolve(employeesTemp); // call "resolve" because we have completed the function successfully
+        }
+    });
+}
+
+module.exports.getDepartments = function () {
+    return new Promise((resolve, reject) => { // place our code inside a "Promise" function
+        if (departments.length == 0) {
+            reject("no results returned");
+        } else {
+            resolve(departments); // call "resolve" because we have completed the function successfully
+        }
+    });
+}
--- server.js
+++ server.js
@@ -1,33 +1,95 @@
 /*********************************************************************************
-*		WEB322	– Assignment 02
+*		WEB322	– Assignment 03
 *		I	declare	that	this	assignment	is	my	own	work	in	accordance	with	Seneca		Academic	Policy.		No	part	
 *		of	this	assignment	has	been	copied	manually	or	electronically	from	any	other	source	
 *		(including	3rd	party	web	sites)	or	distributed	to	other	students.
 *	
-*		Name:	Fernando Henrique Zavalloni Proto	Student	ID:	128133154	Date:	26/May/17
+*		Name:	Fernando Henrique Zavalloni Proto	Student	ID:	128133154	Date:	07/Jun/17
 *
 *		Online	(Heroku)	Link:	https://blooming-brushlands-58927.herokuapp.com/
 *
-********************************************************************************/	
-
+********************************************************************************/
+var dataService = require("./data-service.js");
 var express = require("express");
 var app = express();
 var path = require("path");
 
-var HTTP_PORT = process.env.PORT || 8080;
+var HTTP_PORT = process.env.PORT || 8081;
 
-function onHttpStart(){
+function onHttpStart() {
     console.log("Express http server listening on: " + HTTP_PORT);
 }
 
-app.get("/", function(req, res){
-     res.sendFile(path.join(__dirname + "/views/home.html"));
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname + "/views/home.html"));
 })
 
-app.get("/about", function(req, res){
-     res.sendFile(path.join(__dirname + "/views/about.html"));
+app.get("/about", function (req, res) {
+    res.sendFile(path.join(__dirname + "/views/about.html"));
 })
 
-app.use(express.static('public'));	
 
-app.listen(HTTP_PORT, onHttpStart);
+app.get("/employees", (req, res) => {
+    if (req.query.status) {
+        dataService.getEmployeesByStatus(req.query.status).then((employeesByStatus) => {
+            res.json(employeesByStatus);
+        }).catch((errorMessage) => {
+            res.json({ message: errorMessage });
+        });;
+    } else if (req.query.manager) {
+        dataService.getEmployeesByManager(req.query.manager).then((employeesByManager) => {
+            res.json(employeesByManager);
+        }).catch((errorMessage) => {
+            res.json({ message: errorMessage });
+        });;
+    } else if (req.query.department) {
+        dataService.getEmployeesByDepartment(req.query.department).then((employeesByDepartment) => {
+            res.json(employeesByDepartment);
+        }).catch((errorMessage) => {
+            res.json({ message: errorMessage });
+        });;
+    } else {
+        dataService.getAllEmployees().then((employees) => {
+            res.json(employees);
+        }).catch((errorMessage) => {
+            res.json({ message: errorMessage });
+        });
+    }
+});
+
+app.get("/employee/:empNum", (req, res) => {
+    dataService.getEmployeeByNum(req.params.empNum).then((employee) => {
+        res.json(employee);
+    }).catch((errorMessage) => {
+        res.json({ message: errorMessage });
+    });
+});
+
+app.get("/managers", (req, res) => {
+    dataService.getManagers().then((managers) => {
+        res.json(managers);
+    }).catch((errorMessage) => {
+        res.json({ message: errorMessage });
+    });
+});
+
+app.get("/departments", (req, res) => {
+    dataService.getDepartments().then((departments) => {
+        res.json(departments);
+    }).catch((errorMessage) => {
+        res.json({ message: errorMessage });
+    });
+});
+
+
+app.use((req, res) => {
+    res.status(404).send("Page Not Found");
+});
+
+
+dataService.initialize().then(() => {
+    app.listen(HTTP_PORT, onHttpStart);
+}).catch((err) => {
+    console.log("error: " + err);
+});
+
